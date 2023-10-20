ae348393bf17097b7ed1206ca2ff8e05d049ccc8
mariekart42 <schwokelbaer@gmail.com>
2023-10-20 16:18:27 +0200

    added docker-compose file, trying to connect to postgresql rn

--- test-lol/src/app.module.ts
+++ test-lol/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import {BookmarkModule} from "./bookmark/bookmark.module";
 import {UserModule} from "./User/User.module";
+import {AuthModule} from "./auth/auth.module";
 
 @Module({
-  imports: [AppModule, BookmarkModule, UserModule],
+  imports: [AppModule, BookmarkModule, UserModule, AuthModule],
 })
 export class AppModule {}
