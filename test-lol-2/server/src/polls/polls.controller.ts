63e931a9b75081b1bc0e633f2592cf4f4c8a6e2d
mariekart42 <schwokelbaer@gmail.com>
2023-11-01 16:49:13 +0100

    creating userIDS and responding to POST request in test2

--- test-lol-2/server/src/polls/polls.controller.ts
+++ test-lol-2/server/src/polls/polls.controller.ts
@@ -12,7 +12,7 @@ export class PollsController {
     async create(@Body() createPollDtoObject: CreatePollDto) {
         Logger.log(' -->  POLLS  CREATE');
         const result = await this.pollServiceObject.createPoll(createPollDtoObject);
-
+    console.log(result);
         return result;
     }
 
@@ -20,6 +20,7 @@ export class PollsController {
     async join(@Body() joinPollDtoObject: JoinPollDto) {
         Logger.log(' -->  POLLS  JOIN');
         const result = await this.pollServiceObject.joinPoll(joinPollDtoObject);
+    console.log(result);
         return result;
     }
 
@@ -32,7 +33,9 @@ export class PollsController {
             userID: "Dummy userID",
             // creating a dummy here cause we don't have logic for this yet
         });
+    console.log(result);
         return result;
+
     }
 
 }
