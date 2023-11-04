091b0af0c61a8b9446e68e41013d954177af30e6
mariekart42 <schwokelbaer@gmail.com>
2023-11-04 22:32:43 +0100

    lol

--- TEST_ENVIRONMENT/testing_stage.sh
+++ TEST_ENVIRONMENT/testing_stage.sh
@@ -7,7 +7,7 @@ YEL='\033[0;33m'
 GRN='\033[0;32m'
 RESET='\033[0m' # No Color (reset color back to normal)
 
-
+source ../virtualEnvironment/bin/activate
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install Django
-
+python3 backend/manage.py runserver
