81c0ced498f21c6de26a85083881d8fbe06fdf04
mariekart42 <schwokelbaer@gmail.com>
2023-11-04 22:29:57 +0100

    lol

---
+++ TEST_ENVIRONMENT/testing_stage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define color codes
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+YEL='\033[0;33m'
+GRN='\033[0;32m'
+RESET='\033[0m' # No Color (reset color back to normal)
+
+
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install Django
+
