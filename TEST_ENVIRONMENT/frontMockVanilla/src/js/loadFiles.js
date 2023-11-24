f70515482ec7726488628249bf0a5dfd55f2b6d9
mariekart42 <marie.a.mensing@gmail.com>
2023-11-24 12:39:26 +0100

    loading HTML file into another HTML file works now, user can login

--- TEST_ENVIRONMENT/frontMockVanilla/src/js/loadFiles.js
+++ TEST_ENVIRONMENT/frontMockVanilla/src/js/loadFiles.js
@@ -1,43 +1,30 @@
-        // // Function to load HTML content into a div
-        //
-        // function loadHTMLIntoDiv(url, targetId) {
-        //     fetch(url)
-        //         .then(response => response.text())
-        //         .then(html => {
-        //             document.getElementById(targetId).innerHTML = html;
-        //         })
-        //         .catch(error => console.error('Error loading HTML:', error));
-        // }
-        //
-        // // Call the function with the URL of the HTML file and the ID of the target div
-        // loadHTMLIntoDiv('html/userIsNotAuth.html', 'userIsNotAuth');
-        //
-        //
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Function to load HTML content from file and inject it into a target element
-    function loadContent(file, targetId) {
-        fetch(file)
-            .then(response => response.text())
-            .then(html => {
-                document.getElementById(targetId).innerHTML = html;
+  function loadContent(file, targetId) {
+    fetch(file)
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById(targetId).innerHTML = html;
+        addEventListenersNotAuth();
+      })
+      .catch(error => console.error('Error loading content:', error));
+  }
+  loadContent('html/userIsNotAuth.html', 'userIsNotAuth');
 
-                // If you need to add JavaScript functionality, do it here.
-                // For example, add event listeners after the content is loaded.
-                addEventListeners();
-            })
-            .catch(error => console.error('Error loading content:', error));
-    }
 
-    // Example: Load content into 'userIsNotAuth' div
-    loadContent('html/userIsNotAuth.html', 'userIsNotAuth');
+  function loadContentIsAuth(file, targetId) {
+  fetch(file)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById(targetId).innerHTML = html;
 
-    // Example: Load content into 'userIsAuth' div
-    // loadContent('html/userIsNotAuth.html', 'userIsAuth');
+      // addEventListeners();
+    })
+    .catch(error => console.error('Error loading content:', error));
+  }
+  loadContentIsAuth('html/userIsAuth.html', 'userIsAuth');
 });
 
-function addEventListeners() {
-    // Add your event listeners or other JavaScript functionality here.
-}
+// function addEventListeners() {
+//     // Add your event listeners or other JavaScript functionality here.
+// }
--- TEST_ENVIRONMENT/frontMockVanilla/src/js/login_register.js
+++ TEST_ENVIRONMENT/frontMockVanilla/src/js/login_register.js
@@ -1,33 +1,18 @@
-let obj = {
-    updateUserDataFlag: false,
-    showChatFlag: false,
-    name: "",  // Make sure to initialize the name property
-    password: "",
-    new_name: "",
-    new_age: "",
-    new_password: "",
-    userData: null,
-    errorMessage: null, // New variable to track error messages
-    loginColor: 'green',
-    errorColor: 'red',
-    isLoggedIn: false,
-    showImageFlag: false,
-    imageUrl: "images/moon_dog.jpg",
-    showPage: 'login'
-}
+// let obj = {
+//     name: "",  // Make sure to initialize the name property
+//     password: "",
+// }
 
-
-function addEventListeners() {
+function addEventListenersNotAuth() {
 //   // Your script here
   document.getElementById('loginUserButton').addEventListener('click', function () {
-    const username = document.getElementById('loginUsernameID').value;
-    const password = document.getElementById('loginPasswordID').value;
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
 
     const url = `http://127.0.0.1:6969/user/check_user_credentials/${username}/${password}/`
     fetch(url)
       .then(response => {
         if (!response.ok) {
-          console.log('LOGIN NOT OK RESPONSE:', response);
           alert('Credentials are wrong');
           throw new Error('Credentials are wrong');
         }
@@ -44,113 +29,20 @@ function addEventListeners() {
         console.error('Error during login:', error);
       });
   });
+
+
+  // Event listener for changing to the login page
+  document.getElementById('changeToLoginPageButton').addEventListener('click', function () {
+    const loginPage = document.getElementById('loginPage');
+    const registerPage = document.getElementById('registerPage');
+
+    // Function to show the login page
+    function showLoginPage() {
+      loginPage.classList.remove('hidden');
+      registerPage.classList.add('hidden');
+    }
+
+    // Attach the showLoginPage function to the button click event
+    document.getElementById('changeToLoginPageButton').addEventListener('click', showLoginPage);
+  });
 }
-
-
-//
-// function addEventListeners() {
-//     // Add your event listeners here.
-//     document.getElementById('loginUserButton').addEventListener('click', function () {
-//         // Handle button click
-//     });
-// }
-
-
-
-// // document.addEventListener('DOMContentLoaded', function () {
-//     document.getElementById('loginUserButton').addEventListener('submit', function (event) {
-// console.log('IN DOCU')
-//         // Get the value from the input field
-//         const name = document.getElementById('loginUsernameID').value;
-//         const password = document.getElementById('loginPasswordID').value;
-//
-//         const loginUserButton = document.getElementById('loginUserButton');
-//         loginUserButton.addEventListener('click', loginUser);
-//
-//         // loginUser(name, password)
-//     // });
-// });
-
-//
-//
-// // Wait for the DOM to be ready
-// document.addEventListener('DOMContentLoaded', function () {
-//     // Get references to login and register sections
-//     const loginPage = document.getElementById('loginPage');
-//     const registerPage = document.getElementById('registerPage');
-//
-//     // Get references to buttons
-//     const loginButton = document.getElementById('loginButton');
-//     const registerButton = document.getElementById('registerButton');
-//
-//
-//     const loginUserButton = document.getElementById('loginUserButton');
-//     const registerUserButton = document.getElementById('registerUserButton');
-//
-//     // Get reference of userIsAuth or userIsNotAuth
-//     const userIsNotAuthPage = document.getElementById('userIsNotAuth');
-//     const userIsAuthPage = document.getElementById('userIsAuth');
-//
-//     // Function to show login page and hide register page
-//     function showLoginPage() {
-//         loginPage.classList.remove('hidden');
-//         registerPage.classList.add('hidden');
-//     }
-//     // Function to show register page and hide login page
-//     function showRegisterPage() {
-//         loginPage.classList.add('hidden');
-//         registerPage.classList.remove('hidden');
-//     }
-//     function authUser(){
-//         userIsNotAuthPage.classList.add('hidden');
-//         userIsAuthPage.classList.remove('hidden');
-//     }
-//     // Add click event listeners to buttons
-//     loginButton.addEventListener('click', showLoginPage);
-//     registerButton.addEventListener('click', showRegisterPage);
-//     // loginUserButton.addEventListener('click', loginUser);
-//     // registerUserButton.addEventListener('click', registerUser);
-//
-// });
-//
-//
-
-async function loginUser(name, password) {
-  try
-  {
-    if (!name.trim())
-    {
-      this.errorMessage = 'Please enter ur username!';
-      return;  // Do not proceed with the API request if the input is empty
-    }
-    if (!password.trim())
-    {
-      this.errorMessage = 'Please enter ur password!';
-      return;
-    }
-    const response = await fetch(`http://127.0.0.1:6969/user/data/get/${name}/${password}/`);
-    const data = await response.json();
-    if (response.ok)
-    {
-      console.log('RESPONSE OK')
-      this.errorMessage = null;
-      this.isLoggedIn = true;
-      this.userData = data;
-    }
-    else
-    {
-      console.log('RESPONSE NOT OK')
-      this.errorMessage = data.error;
-    }
-  }
-  catch (error)
-  {
-    console.log('RESPONSE NOT OK')
-    console.error('Error fetching user data:', error);
-    this.errorMessage = 'Error fetching user data';
-  }
-}
-// // function loginUser() {
-// //
-// // }
-//
