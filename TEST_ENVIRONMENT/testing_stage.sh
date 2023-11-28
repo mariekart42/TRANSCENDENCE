26dcb75203a8e9df6e8d925d875a228ae2c6f41e
mariekart42 <marie.a.mensing@gmail.com>
2023-11-28 21:20:38 +0100

    updated bash script

--- TEST_ENVIRONMENT/testing_stage.sh
+++ TEST_ENVIRONMENT/testing_stage.sh
@@ -1,65 +1,51 @@
 #!/bin/bash
 
-# Define color codes
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 YEL='\033[0;33m'
 GRN='\033[0;32m'
-RESET='\033[0m' # No Color (reset color back to normal)
+RESET='\033[0m'
 
-# Prompt the user
+
 echo -e " 🔔  ${YEL}ARE YOU IN VIRTUAL ENVIRONMENT?${RESET}"
 read -p "     [Y|N|?]: " response
 
-# Check the user's response
 if [[ "$response" == "y" || "$response" == "Y" ]]; then
 
-    echo -e " 🗿  ${BLUE}Upgrading pip...${RESET}"
-    python3 -m pip install --upgrade pip
+  echo -e " 🗿  ${BLUE}Upgrading pip...${RESET}"
+  python3 -m pip install --upgrade pip
 
-    echo -e " 🗿  ${BLUE}Installing Django...${RESET}"
-    python3 -m pip install Django
-    python3 -m pip install --upgrade django
+  echo -e " 🗿  ${BLUE}Installing Django...${RESET}"
+  python3 -m pip install Django
+  python3 -m pip install --upgrade django
 
+  python3 -m pip install channels
+  python3 -m pip install --upgrade channels
 
-    python3 -m pip install channels
-    python3 -m pip install --upgrade channels
-
-    python3 -m pip install daphne
-    python3 -m pip install --upgrade daphne
-
-#    echo -e " 🗿  ${BLUE}Installing Vue CLI...${RESET}"
-#    npm install --save-dev @vue/cli
-#    npm install --save-dev @vue/cli-service
+  python3 -m pip install daphne
+  python3 -m pip install --upgrade daphne
 
 
-        # Prompt the user to run migrations
-#    echo -e " 🔔  ${YEL}DO YOU WANT TO MIGRATE CHANGES TO DATABASE?${RESET} (optional but recommended)"
-#    read -p "     [Y/N]: " migrate_response
+  echo -e " 🗿  ${BLUE}Applying migrations...${RESET}"
+  python3 backend/manage.py makemigrations
+  python3 backend/manage.py migrate
 
-#    if [[ "$migrate_response" == "y" || "$migrate_response" == "Y" ]]; then
-        echo -e " 🗿  ${BLUE}Applying migrations...${RESET}"
 
-        python3 backend/manage.py makemigrations
-        python3 backend/manage.py migrate
+  echo -e " 🗿  ${BLUE}Starting Django server...${RESET}"
+  python3 backend/manage.py runserver 6969
 
-#    else
-#        echo -e " ⚠️  ${YEL}Migrations skipped${RESET}"
-#    fi
 
-    echo -e " 🗿  ${BLUE}Starting Django server...${RESET}"
-    # u can specify port at the end, default is 8000
-    python3 backend/manage.py runserver 6969
-
-    echo -e " 🗿  ${RED}Server exit${RESET}"
+  echo -e " 🗿  ${RED}Server exit${RESET}"
 
 elif [[ "$response" == "?" ]]; then
-    echo -e "\n 💡  ${RESET}Your Terminal should look like this:"
-    echo -e "       (${YEL}virtualEnvironment${RESET}) user@lol69 TRANSCENDENCE %"
-    echo -e "\n     To activate virtual Environment run:"
-    echo -e "       ${YEL}source virtualEnvironment/bin/activate${RESET}"
-    echo -e "\n     For more Information's go to:"
-    echo -e "       https://docs.python.org/3/library/venv.html\n"
+  echo -e "\n 💡  ${RESET}Your Terminal should look like this:"
+  echo -e "       (${YEL}virtualEnvironment${RESET}) user@lol69 TRANSCENDENCE %"
+  echo -e "\n     To create virtual Environment:"
+  echo -e "       ${YEL}python3 -m venv <venv_name>${RESET}"
+  echo -e "\n     To activate virtual Environment run:"
+  echo -e "       ${YEL}source <venv_name>/bin/activate${RESET}"
+  echo -e "\n     For more Information's go to:"
+  echo -e "       https://docs.python.org/3/library/venv.html\n"
 else
-    echo -e "\n ${RED}❌   ACTIVATE VIRTUAL ENVIRONMENT!\n${BLUE} 💡  RUN:  source virtualEnvironment/bin/activate${RESET}"
+  echo -e "\n ${RED}❌   ACTIVATE VIRTUAL ENVIRONMENT!\n${BLUE} 💡  RUN:  source virtualEnvironment/bin/activate${RESET}"
 fi
