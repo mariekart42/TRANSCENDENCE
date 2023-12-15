1e0605cb2107508de9381e6da3c0b94062ca176c
mariekart42 <marie.a.mensing@gmail.com>
2023-12-15 13:25:46 +0100

    implemented navbar in html/js

--- TEST_ENVIRONMENT/frontMockVanilla/src/js/user_is_auth.js
+++ TEST_ENVIRONMENT/frontMockVanilla/src/js/user_is_auth.js
@@ -17,6 +17,40 @@ function addEventListenersIsAuth() {
     const chat = document.getElementById('chat')
     chat.classList.remove('hidden')
   })
+  document.getElementById('nothingButton').addEventListener('click', function () {
+    const chat = document.getElementById('chat')
+    chat.classList.add('hidden')
+    const nothingSite = document.getElementById('nothingSite')
+    nothingSite.classList.remove('hidden')
+  })
+  document.getElementById('profileButton').addEventListener('click', function () {
+    const chat = document.getElementById('chat')
+    chat.classList.add('hidden')
+    const nothingSite = document.getElementById('nothingSite')
+    nothingSite.classList.add('hidden')
+
+
+    const userModal = new bootstrap.Modal(document.getElementById('userProfileModal2'));
+
+    const label = document.getElementById('userModalLabel2');
+    label.innerHTML = `<h5>${websocket_obj.username}</h5>`;
+
+    // Populate the modal body with user data
+    const modalBody = document.getElementById('userProfileModalBody2');
+    modalBody.innerHTML = `<p>Name: ${websocket_obj.username}</p><p>ID: ${websocket_obj.user_id}</p>`;
+
+    // Show the modal
+    userModal.show();
+
+
+  })
+  document.getElementById('home').addEventListener('click', function () {
+    const chat = document.getElementById('chat')
+    chat.classList.add('hidden')
+    const nothingSite = document.getElementById('nothingSite')
+    nothingSite.classList.add('hidden')
+    // also game etc. later
+  })
 }
 
 
