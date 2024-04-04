3b1d19feeeacb3cd86d4ff53c0c9347af2dc0bd3
vfuhlenb <vfuhlenb@students.42wolfsburg.de>
2024-04-04 19:08:44 +0200

    testing decorator

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+JWT_SECRET = os.environ.get('JWT_SECRET')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # == /Users/mmensing/Desktop/CODE/TRANSCENDENCE/TEST_ENVIRONMENT/backend
